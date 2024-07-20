1976858eadd2216885ab747a359a3c1f163d9f04
Usama <osamasayed585@gmai.com>
2024-07-20 17:38:52 +0300

    Added Hilt dependency and updated project configuration. Updated unit test to reflect changes. Added Constants object for storing project- wide constants.

---
+++ app/src/main/java/com/example/kotlinpractice/presentation/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinpractice.presentation.util
+
+object Constants {
+
+    const val BASE_URL = ""
+    const val PREFERENCES_STORE_NAME = ""
+    const val INITIAL_PAGE = 1
+    const val TITLE_MAX_LINES = 2
+    const val DESCRIPTION_MAX_LINES = 2
+    const val PAGE_SIZE = 10
+}
